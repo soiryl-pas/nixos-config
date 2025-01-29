b44fe72811d284c501ad8f113d567f9c316b7700
Someone0123-pas <Someone0123@gmx.at>
2025-01-29 15:19:14 +0100

    18 current  2025-01-29 15:19:11  24.11.20250126.4e96537     6.6.72                          *

--- global/gui.nix
+++ global/gui.nix
@@ -3,6 +3,7 @@
 {
   services = {
     xserver.enable = true;
+    xserver.displayManager.startx.enable = true;
     # displayManager.sddm.enable = false;
     desktopManager.plasma6.enable = true;
   };
