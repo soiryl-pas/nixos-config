71a916e6f3227c63737d0a72b5c0a7458376b57e
Someone0123-pas <Someone0123@gmx.at>
2025-01-29 15:50:17 +0100

    20 current  2025-01-29 15:50:15  24.11.20250126.4e96537     6.6.72                          *

--- global/default.nix
+++ global/default.nix
@@ -6,8 +6,6 @@
     ./gui.nix
   ];
 
-  default_display_manager = "sddm";
-
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
@@ -71,6 +69,8 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "input" "networkmanager" "vboxsf" ];
   };
+
+  default_display_manager = "none";
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
