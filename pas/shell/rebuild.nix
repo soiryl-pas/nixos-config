f9b94bd4b696274a92356cb817e4f9205e18b33f
Someone0123-pas <Someone0123@gmx.at>
2025-01-30 16:45:14 +0100

    67 current  2025-01-30 16:45:11  24.11.20250126.4e96537     6.6.72                          *

--- pas/shell/rebuild.nix
+++ pas/shell/rebuild.nix
@@ -1,22 +1,19 @@
 { pkgs }:
 
-let
-  git = pkgs.git;
-in
 pkgs.writeShellApplication {
   name = "rebuild";
   text = ''
     pushd "$HOME"/nixos-configuration
 
     echo "== Git diff =="
-    ${git} diff -U0 --color=always | less -R
+    git diff -U0 --color=always | less -R
     echo -e "\n== Git add =="
-    ${git} add ./* || true
+    git add ./* || true
     echo -e "\n== Switching to Nixos with Configuration in ~/nixos-configuration/ =="
     sudo nixos-rebuild switch --flake . 2>&1 | tee nixos-last-switch.log
     echo -e "\n== Committing to git =="
     gen=$(nixos-rebuild list-generations | grep current)
-    ${git} commit -am "$gen"
+    git commit -am "$gen"
 
     popd
   '';
