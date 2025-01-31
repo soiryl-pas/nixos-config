b7aee4e2d48432855d1871877a565631acd4182f
Someone0123-pas <Someone0123@gmx.at>
2025-01-31 14:36:34 +0100

    99 current  2025-01-31 14:35:10  24.11.20250126.4e96537     6.6.72                          *

--- global/gui.nix
+++ global/gui.nix
@@ -38,7 +38,7 @@ in
       xserver.displayManager.startx.enable = (cfg == "none");
       displayManager.sddm = rec { 
 	enable = (cfg == "sddm");
-	wayland.enable = true;
+	#wayland.enable = true;
       };
       greetd = {
 	enable = (cfg == "greetd");
