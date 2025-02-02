e949e087afd260824ff003ed50f9a6405f0c2f9e
Someone0123-pas <Someone0123@gmx.at>
2025-02-02 10:34:43 +0100

    138 current  2025-02-02 10:34:40  24.11.20250130.666e1b3  6.6.74                          *

--- pas/programs/nvim.nix
+++ pas/programs/nvim.nix
@@ -11,10 +11,12 @@
         languages = {
           enableLSP = true;
           enableTreesitter = true;
+
           nix.enable = true;
           clang.enable = true;
           python.enable = true;
           html.enable = true;
+          bash.enable = true;
       };
     };
   };
