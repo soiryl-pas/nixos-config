b5430171d7b13d658b318efe3ba38e4ba4d59ec3
Someone0123-pas <Someone0123@gmx.at>
2025-02-02 10:50:21 +0100

    140 current  2025-02-02 10:50:19  24.11.20250130.666e1b3  6.6.74                          *

--- pas/programs/nvf-nvim.nix
+++ pas/programs/nvf-nvim.nix
@@ -7,19 +7,19 @@
 
     settings.vim = {
       treesitter.context.enable = true;
-      
-        options.shiftwidth = 2;
-        languages = {
-          enableLSP = true;
-          enableTreesitter = true;
-          enableFormat = true;
+      options.shiftwidth = 2;
+      lsp.formatOnSave = true;
 
-          nix.enable = true;
-          clang.enable = true;
-          python.enable = true;
-          html.enable = true;
-          bash.enable = true;
-      
+      languages = {
+        enableLSP = true;
+        enableTreesitter = true;
+        enableFormat = true;
+
+        nix.enable = true;
+        clang.enable = true;
+        python.enable = true;
+        html.enable = true;
+        bash.enable = true;
     };
   };
 
