2eb92a71acafa3ef8513f293900047185e29e9b8
Someone0123-pas <Someone0123@gmx.at>
2025-02-02 10:21:20 +0100

    134 current  2025-02-02 10:21:19  24.11.20250130.666e1b3  6.6.74                          *

--- pas/programs/nvim.nix
+++ pas/programs/nvim.nix
@@ -4,5 +4,18 @@
   programs.nvf = {
     enable = true;
     enableManpages = true;
+
+    settings = {
+      vim.options.shiftwidth = 2;
+    };
   };
+
+  /*
+  vim.languages = {
+    nix.enable = true;
+    clang.enable = true;
+    python.enable = true;
+    html.enable = true;
+  };
+  */
 }
