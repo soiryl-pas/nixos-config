4371f2ffcffa026bde5f338291ba9cf6f0bef4d3
Someone0123-pas <Someone0123@gmx.at>
2025-02-06 13:05:57 +0100

    188 current  2025-02-06 13:05:52  24.11.20250201.f668777  6.6.74                          *

--- pas/shell/cpp.nix
+++ pas/shell/cpp.nix
@@ -2,13 +2,20 @@
 
 {
   home.packages = [
-    ( pkgs.runCommand "cpp" {
-      buildInputs = with pkgs; [
-        clang
-      ];
-      #nativeBuildInputs = [ pkgs.makeWrapper ];
-    } ''
-      clang++ -o main "$@"
-    '' )
+    ( let 
+        buildInputs = with pkgs; [
+          clang
+          bash
+        ];
+      in
+        pkgs.runCommand "cpp" {
+        inherit buildInputs;
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+      } ''
+        mkdir -p $out/bin/
+        ln -s ${pkgs.bash}/bin/bash $out/bin/cpp-shell
+        wrapProgram $out/bin/cpp-shell --prefix PATH : ${pkgs.lib.makeBinPath buildInputs}
+      ''
+    )
   ];
 }
--- pas/shell/default.nix
+++ pas/shell/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./bash.nix
+    ./cpp.nix
   ];
 }
