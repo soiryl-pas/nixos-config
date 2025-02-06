b2ad58bf0fe55a1746b4f30d660bd7602f5352bb
Someone0123-pas <Someone0123@gmx.at>
2025-02-06 20:37:09 +0100

    200 current  2025-02-06 20:19:08  24.11.20250204.030ba19  6.6.75                          *

--- pas/shell/cpp/flake.nix
+++ pas/shell/cpp/flake.nix
@@ -17,7 +17,8 @@
         ];
 
         shellHook = ''
-          export PS1="\n \e[1;34m== CPP: \w ==\n $\e[m "
+          export PS1="\n \[\e[1;34m\]== CPP: \w ==\n $\[\e[m\] "
+          alias cpp="clang++ -Wall -Wextra -std=c++17 -O3 -pedantic-errors -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all"
           echo -e "\nclang++ `${pkgs.clang}/bin/clang++ --version`"
         '';
       };
