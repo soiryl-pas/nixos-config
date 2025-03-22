79859694d4e57c2c339931900bdfde0d0e104eb3
Someone0123-pas <Jemand0123@protonmail.com>
2025-03-22 21:46:07 +0100

    227 current  2025-03-22 21:46:33  24.11.20250320.7105ae3  6.6.83                          *

--- global/gui.nix
+++ global/gui.nix
@@ -35,8 +35,7 @@ in
     security.pam.services.login.kwallet.forceRun = true;
     systemd.user.services.kwallet-pam-unlock = {
       enable = true;
-      after = [ "basic.target" ];
-      wantedBy = [ "default.target" ];
+      after = [ "default.target" ];
       description = "Unlocks kwallet with pam credentials, even if other plasma services aren't running";
       serviceConfig = {
 	Type = "simple";
