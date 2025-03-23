16c739922bedf36a249d2aca26b98684f2d9fc9f
Someone0123-pas <Jemand0123@protonmail.com>
2025-03-23 18:04:20 +0100

    239 current  2025-03-23 18:04:17  24.11.20250320.7105ae3  6.6.83                          *

--- global/gui.nix
+++ global/gui.nix
@@ -37,10 +37,10 @@ in
       enable = true;
       wantedBy = [ "default.target" ];
       description = "Unlocks kwallet with pam credentials, even if other plasma services aren't running";
-      #environment = { QT_QPA_PLATFORM = "offscreen"; };
+      environment = { QT_QPA_PLATFORM = "offscreen"; };
       serviceConfig = {
 	Type = "simple";
-	ExecStart = "/usr/bin/env QT_QPA_PLATFORM=offscreen ${config.security.pam.services.login.kwallet.package}/libexec/pam_kwallet_init";
+	ExecStart = "${config.security.pam.services.login.kwallet.package}/libexec/pam_kwallet_init";
       };
     };
 
