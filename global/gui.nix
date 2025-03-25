28b49424423b6df6abcad70081337f5fee86a3c5
Someone0123-pas <Jemand0123@protonmail.com>
2025-03-25 06:18:37 +0100

    257 current  2025-03-25 06:18:33  24.11.20250320.7105ae3  6.6.83                          *

--- global/gui.nix
+++ global/gui.nix
@@ -33,7 +33,7 @@ in
     
     # Enable kwallet-pam for tty and qtile sessions as well, depends on Plasma enabling pam.service.login.kwallet
     security.pam.services.login.kwallet.forceRun = true;
-    environment.loginShellInit = "QT_QPA_PLATFORM=offscreen ${config.security.pam.services.login.kwallet.package}/libexec/pam_kwallet_init > /dev/null";
+    environment.loginShellInit = "QT_QPA_PLATFORM=offscreen ${config.security.pam.services.login.kwallet.package}/libexec/pam_kwallet_init &> /dev/null";
 
     services = {
       # Xorg Conf
