ddceb729c7a8fb4e31ca5f3ae96b3810f8ced23a
Someone0123-pas <Jemand0123@protonmail.com>
2025-03-27 20:27:45 +0100

    265 current  2025-03-27 20:27:43  24.11.20250323.f0946fa  6.6.83                          *

--- pas/shell/rebuild.nix
+++ pas/shell/rebuild.nix
@@ -12,7 +12,7 @@ pkgs.writeShellApplication {
     echo -e "\n== Switching to Nixos with Configuration in ~/nixos-configuration/ =="
     sudo nixos-rebuild switch --flake . 2>&1 | tee nixos-last-switch.log
     echo -e "\n== Committing to git =="
-    gen=$(nixos-rebuild list-generations | grep current)
+    gen=$(nixos-rebuild list-generations | grep current | awk '{ print $1,$3,$4,$5,$6 }')
     git commit -am "$gen"
 
     popd
