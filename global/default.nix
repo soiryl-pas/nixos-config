32c430e203565a063be6d667324e7b2c19f06749
Someone0123-pas <Jemand0123@protonmail.com>
2025-03-27 21:49:32 +0100

    269 2025-03-27 21:49:28 24.11.20250325.d02d88f 6.6.84

--- global/default.nix
+++ global/default.nix
@@ -40,7 +40,12 @@
 
   time.timeZone = "Europe/Vienna";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "de_AT.UTF-8";
+    };
+  };
   console = {
     font = "Lat2-Terminus16";
     useXkbConfig = true;
