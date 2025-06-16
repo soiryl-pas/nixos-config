d7008cfc6987ba7321c512713b3e5f1f08dc7340
Someone0123-pas <Jemand0123@protonmail.com>
2025-06-16 16:00:53 +0200

    113 2025-06-16 16:00:51 24.11.20250528.78add7b 6.6.92

--- global/default.nix
+++ global/default.nix
@@ -73,6 +73,17 @@
 	fcitx5-tokyonight
       ];
       fcitx5.waylandFrontend = true;
+      fcitx5.settings.inputMethod = {
+	GroupOrder."0" = "Default";
+	"Groups/0" = {
+	  Name = "Default";
+	  "Default Layout" = "de-e1";
+	  DefaultIM = "mozc";
+	};
+	"Groups/0/Items/0".Name = "keyboard-de-e1";
+	"Groups/0/Items/1".Name = "mozc";
+	"Groups/0/Items/2".Name = "pinyin";
+      };
     };
   };
 
