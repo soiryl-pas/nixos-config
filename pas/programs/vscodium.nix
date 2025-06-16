0043129242484538eb9ebb8b2ebdb348ad486570
Someone0123-pas <Jemand0123@protonmail.com>
2025-06-16 18:35:09 +0200

    114 2025-06-16 18:25:43 25.05.20250615.6c64dab 6.12.33

--- pas/programs/vscodium.nix
+++ pas/programs/vscodium.nix
@@ -4,83 +4,85 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      ms-python.python
-      ms-python.vscode-pylance
-      xaver.clang-format
-      ms-vscode.cpptools
-      mkhl.direnv
-      dracula-theme.theme-dracula
-      ms-vscode.hexeditor
-      twxs.cmake
-      ms-vscode.cmake-tools
-      redhat.java
-      vscjava.vscode-maven
-      vscjava.vscode-gradle
-      vscjava.vscode-java-test
-      vscjava.vscode-java-debug
-      vscjava.vscode-java-dependency
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-	name = "ARM";
-	publisher = "dan-c-underwood";
-	version = "1.4.0";
-	sha256 = "sha256-atDbLgc2R+X/wyGAxA6M0lDa03L5yFX1rbfpyZ8oKUU=";
-      }
-      {
-	name = "qtvsctools";
-	publisher = "tonka3000";
-	version = "0.11.0";
-	sha256 = "sha256-/iJzPI4xJY+Vg9B/ah+zdErq988aXdN/UL1V3fR2nJ8=";
-      }
-    ];
-    keybindings = [
-      {
-	key = "f1";
-	command = "workbench.action.quickOpenWithModes";
-      }
-      {
-	key = "shift+f1";
-	command = "workbench.action.copyEditorToNewWindow";
-	when = "activeEditor";
-      }
-      {
-	key = "f4";
-	command = "workbench.action.terminal.toggleTerminal";
-      }
-      {
-	key = "shift+f4";
-	command = "workbench.action.terminal.openNativeConsole";
-	when = "!terminalFocus";
-      }
-      {
-	key = "ctrl+o";
-	command = "workbench.action.openWalkthrough";
-      }
-      {
-	key = "ctrl+shift+space";
-	command = "editor.action.triggerSuggest";
-	when = "editorHasCompletionItemProvider && textInputFocus && !editorReadonly";
-      }
-      {
-	key = "ctrl+shift+f12";
-	command = "editor.action.revealDeclaration";
-      }
-      {
-	key = "ctrl+[Backslash]";
-	command = "direnv.reset";
-      }
-    ];
-    userSettings = {
-      "editor.defaultFormatter" = "xaver.clang-format";
-      "git.confirmSync" = false;
-      "git.autofetch" = true;
-      "workbench.colorTheme" = "Dracula Theme";
-      "direnv.restart.automatic" = true;
-      "debug.allowBreakpointsEverywhere" = true;
-      "vim.useSystemClipboard" = true;
-      "redhat.telemetry.enabled" = false;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+	vscodevim.vim
+	ms-python.python
+	ms-python.vscode-pylance
+	xaver.clang-format
+	ms-vscode.cpptools
+	mkhl.direnv
+	dracula-theme.theme-dracula
+	ms-vscode.hexeditor
+	twxs.cmake
+	ms-vscode.cmake-tools
+	redhat.java
+	vscjava.vscode-maven
+	vscjava.vscode-gradle
+	vscjava.vscode-java-test
+	vscjava.vscode-java-debug
+	vscjava.vscode-java-dependency
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+	{
+	  name = "ARM";
+	  publisher = "dan-c-underwood";
+	  version = "1.4.0";
+	  sha256 = "sha256-atDbLgc2R+X/wyGAxA6M0lDa03L5yFX1rbfpyZ8oKUU=";
+	}
+	{
+	  name = "qtvsctools";
+	  publisher = "tonka3000";
+	  version = "0.11.0";
+	  sha256 = "sha256-/iJzPI4xJY+Vg9B/ah+zdErq988aXdN/UL1V3fR2nJ8=";
+	}
+      ];
+      keybindings = [
+	{
+	  key = "f1";
+	  command = "workbench.action.quickOpenWithModes";
+	}
+	{
+	  key = "shift+f1";
+	  command = "workbench.action.copyEditorToNewWindow";
+	  when = "activeEditor";
+	}
+	{
+	  key = "f4";
+	  command = "workbench.action.terminal.toggleTerminal";
+	}
+	{
+	  key = "shift+f4";
+	  command = "workbench.action.terminal.openNativeConsole";
+	  when = "!terminalFocus";
+	}
+	{
+	  key = "ctrl+o";
+	  command = "workbench.action.openWalkthrough";
+	}
+	{
+	  key = "ctrl+shift+space";
+	  command = "editor.action.triggerSuggest";
+	  when = "editorHasCompletionItemProvider && textInputFocus && !editorReadonly";
+	}
+	{
+	  key = "ctrl+shift+f12";
+	  command = "editor.action.revealDeclaration";
+	}
+	{
+	  key = "ctrl+[Backslash]";
+	  command = "direnv.reset";
+	}
+      ];
+      userSettings = {
+	"editor.defaultFormatter" = "xaver.clang-format";
+	"git.confirmSync" = false;
+	"git.autofetch" = true;
+	"workbench.colorTheme" = "Dracula Theme";
+	"direnv.restart.automatic" = true;
+	"debug.allowBreakpointsEverywhere" = true;
+	"vim.useSystemClipboard" = true;
+	"redhat.telemetry.enabled" = false;
+      };
     };
   };
 }
