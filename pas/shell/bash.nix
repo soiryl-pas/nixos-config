622cc25bdcef847b672e8f2652688e6d4ed741ff
Someone0123-pas <Jemand0123@protonmail.com>
2025-07-27 14:30:07 +0200

    155 2025-07-27 14:30:04 25.05.20250713.dfcd5b9 6.12.37

--- pas/shell/bash.nix
+++ pas/shell/bash.nix
@@ -18,7 +18,10 @@
 
     # Further ~/.bashrc Configs
     bashrcExtra = ''
-      export PS1="\n \[\e[1;36m\]\u\[\e[m\] > \[\e[0;90m\]\D{%d.%m.%Y %R}\[\e[m\] > \[\e[0;35m\](\w)\[\e[m\]\n \[\e[1;36m\]$ \[\e[m\]"
+      git_branch() {
+	git branch --show-current 2> /dev/null
+      }
+      export PS1="\n \[\e[1;36m\]\u\[\e[m\] > \[\e[0;90m\]\D{%d.%m.%Y %R}\[\e[m\] > \[\e[0;35m\](\w)\[\e[m\] $(git_branch)\n \[\e[1;36m\]$ \[\e[m\]"
     '';
   };
 }
