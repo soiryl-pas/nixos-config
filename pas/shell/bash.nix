751db58d15ff765c32e24425837d5bc2be57f8d8
Someone0123-pas <Jemand0123@protonmail.com>
2025-07-27 15:00:03 +0200

    165 2025-07-27 15:00:01 25.05.20250713.dfcd5b9 6.12.37

--- pas/shell/bash.nix
+++ pas/shell/bash.nix
@@ -19,7 +19,7 @@
     # Further ~/.bashrc Configs
     # __git_ps1 only works if git is also enabled systemwide (and with git.prompt.enable = true)
     bashrcExtra = ''
-      export PROMPT_COMMAND="\n \[\e[1;36m\]\u\[\e[m\] > \[\e[0;90m\]\D{%d.%m.%Y %R}\[\e[m\] > \[\e[0;32m\]$(__git_ps1 "%s:")\[\e[m\]\[\e[0;35m\](\w)\[\e[m\]\n \[\e[1;36m\]$ \[\e[m\]; $PROMPT_COMMAND"
+      export PROMPT_COMMAND="\n \[\e[1;36m\]\u\[\e[m\] > \[\e[0;90m\]\D{%d.%m.%Y %R}\[\e[m\] > \[\e[0;32m\]$(__git_ps1 "%s:")\[\e[m\]\[\e[0;35m\](\w)\[\e[m\]\n \[\e[1;36m\]$ \[\e[m\]"
     '';
   };
 }
