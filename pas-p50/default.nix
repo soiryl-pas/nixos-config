71fbd8d386437eeab8098d57078e0d592ef8cd24
soiryl-pas <Jemand0123@protonmail.com>
2025-09-01 21:06:16 +0200

    5 2025-09-01 21:00:27 25.05.20250826.4e7667a 6.12.43

--- pas-p50/default.nix
+++ pas-p50/default.nix
@@ -7,6 +7,11 @@
   networking.hostName = "pas-p50";
   services.xserver.dpi = 96;
 
+  hardware.nvidia.prime = {
+    offload.enable = false;
+    sync.enable = true;
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
