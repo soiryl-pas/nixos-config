fac4570989efda915126f090785838ac7aecd9c2
soiryl-pas <Jemand0123@protonmail.com>
2025-09-02 09:31:03 +0200

    8 2025-09-02 09:31:00 25.05.20250826.4e7667a 6.12.43

--- pas-p50/default.nix
+++ pas-p50/default.nix
@@ -10,8 +10,8 @@
 
   hardware.nvidia.open = false;
   hardware.nvidia.prime = {
-    offload.enable = false;
-    sync.enable = true;
+    #offload.enable = false;
+    #sync.enable = true;
   };
 
   # This option defines the first version of NixOS you have installed on this particular machine,
