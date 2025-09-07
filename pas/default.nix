9f8c75b6c6f303ca16b15de961e99a20278a38e5
soiryl-pas <Jemand0123@protonmail.com>
2025-09-07 15:44:23 +0200

    198 2025-09-07 15:44:21 25.05.20250826.4e7667a 6.12.43

--- pas/default.nix
+++ pas/default.nix
@@ -8,30 +8,7 @@
   };
 
   manual.manpages.enable = true;
-
-  i18n.inputMethod.fcitx5.settings.inputMethod = {
-    GroupOrder."0" = "EU";
-    GroupOrder."1" = "DE";
-
-    "Groups/0" = {
-      Name = "DE";
-      "Default Layout" = "de-e1";
-      DefaultIM = "mozc";
-    };
-    "Groups/0/Items/0".Name = "keyboard-de-e1";
-    "Groups/0/Items/1".Name = "mozc";
-    "Groups/0/Items/2".Name = "pinyin";
-    
-    "Groups/1" = {
-      Name = "EU";
-      "Default Layout" = "eu";
-      DefaultIM = "mozc";
-    };
-    "Groups/1/Items/0".Name = "keyboard-eu";
-    "Groups/1/Items/1".Name = "mozc";
-    "Groups/1/Items/2".Name = "pinyin";
-  };
-
+  
   imports = [
     ./programs
     ./shell
