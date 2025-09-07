87536a2d2cfa772ba5413d51f11e59db6303a1cc
soiryl-pas <Jemand0123@protonmail.com>
2025-09-07 15:29:49 +0200

    196 2025-09-07 15:29:47 25.05.20250826.4e7667a 6.12.43

--- global/default.nix
+++ global/default.nix
@@ -72,16 +72,26 @@
       ];
       fcitx5.waylandFrontend = true;
       fcitx5.settings.inputMethod = {
-	GroupOrder."0" = "Default";
+	GroupOrder."0" = "EU";
+        GroupOrder."1" = "DE";
+
 	"Groups/0" = {
-	  Name = "Default";
+	  Name = "DE";
 	  "Default Layout" = "de-e1";
 	  DefaultIM = "mozc";
 	};
-        "Groups/0/Items/0".Name = "keyboard-eu";
+	"Groups/0/Items/0".Name = "keyboard-de-e1";
 	"Groups/0/Items/1".Name = "mozc";
 	"Groups/0/Items/2".Name = "pinyin";
-	"Groups/0/Items/3".Name = "keyboard-de-e1";
+        
+        "Groups/1" = {
+          Name = "EU";
+          "Default Layout" = "eu";
+          DefaultIM = "mozc";
+        };
+        "Groups/1/Items/0".Name = "keyboard-eu";
+	"Groups/1/Items/1".Name = "mozc";
+	"Groups/1/Items/2".Name = "pinyin";
       };
     };
   };
