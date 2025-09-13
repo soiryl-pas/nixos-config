515842c54e9628fe7b61bcdb7fccc5ab5dd35d73
soiryl-pas <Jemand0123@protonmail.com>
2025-09-13 14:56:38 +0200

    211 2025-09-13 14:56:36 25.05.20250826.4e7667a 6.12.43

--- global/default.nix
+++ global/default.nix
@@ -157,6 +157,9 @@
       enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
     };
 
     appimage = {
