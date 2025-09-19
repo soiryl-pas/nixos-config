7e0b2236588706a90d8d78c16335a97168008536
soiryl-pas <Jemand0123@protonmail.com>
2025-09-19 13:57:49 +0200

    218 2025-09-19 13:57:47 25.05.20250826.4e7667a 6.12.43

--- global/default.nix
+++ global/default.nix
@@ -35,6 +35,10 @@
       dates = "weekly";
       options = "--delete-older-than 30d";
     };
+    extraOptions = ''
+      extra-substituters = https://devenv.cachix.org
+      extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+    '';
   };
 
   boot.loader = {
