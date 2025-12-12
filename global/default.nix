b3ddeebc39ed1f2d463f5df3bc6a1a7de698b2ac
soiryl-pas <Jemand0123@protonmail.com>
2025-12-12 21:26:06 +0100

    250 2025-12-12 21:26:04 25.11.20251206.d9bc5c7 6.12.60

--- global/default.nix
+++ global/default.nix
@@ -209,7 +209,7 @@
 
   users.users.pas = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "input" "networkmanager" "vboxsf" "docker" ];
+    extraGroups = [ "input" "uinput" "wheel" "input" "networkmanager" "vboxsf" "docker" ];
   };
 
   hardware = {
