25c7c45b48963896b98a31d41366702c9911ed67
soiryl-pas <Jemand0123@protonmail.com>
2026-05-31 22:07:27 +0200

    307 2026-05-05 15:32:05 25.11.20260421.10e7ad5 6.12.82

--- pas/programs/kitty.nix
+++ pas/programs/kitty.nix
@@ -4,10 +4,11 @@
   programs.kitty = {
     enable = true;
     themeFile = "Dracula";    
+    /*
     font = {
       name = "Unifont";
-      size = 8.0;
     };
+    */
     settings = {
     };
   };
