b8b7ce32efd1a243dfac67eed3f30f465db64ddd
soiryl-pas <Jemand0123@protonmail.com>
2026-06-02 22:26:07 +0200

    12 2026-06-02 22:25:48 26.05.20260531.b51242d 6.18.33

--- pas-whispywoods/default.nix
+++ pas-whispywoods/default.nix
@@ -11,12 +11,10 @@
   networking.hostName = "pas-whispywoods";
   system.stateVersion = "26.05"; # Do not change
 
-  /*
   environment.systemPackages = with pkgs; [
     maliit-keyboard
     squeekboard
   ];
-  */
 
   hardware.sensor.iio.enable = true;
   systemd = {
