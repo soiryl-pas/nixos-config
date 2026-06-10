24a31e30a394438805eb3a0123df260d7c2c1144
soiryl-pas <Jemand0123@protonmail.com>
2026-06-10 11:23:03 +0200

    56 2026-06-10 11:05:14 26.05.20260608.bd0ff2d 6.18.34

--- pas-waddledee/default.nix
+++ pas-waddledee/default.nix
@@ -12,6 +12,8 @@
     reverseSync.enable = true;
   };
 
+  hardware.enableAllFirmware = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
