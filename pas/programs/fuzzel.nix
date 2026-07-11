880cdd60940ac4aa6da865d9036040416a795230
soiryl-pas <Jemand0123@protonmail.com>
2026-07-11 11:51:28 +0200

    334 2026-07-11 11:51:26 26.05.20260608.bd0ff2d 6.18.34

--- pas/programs/fuzzel.nix
+++ pas/programs/fuzzel.nix
@@ -3,16 +3,16 @@
   programs.fuzzel.enable = true;
   programs.fuzzel.settings = {
     colors = {
-      background = "350c44a0";
+      background = "350c44c0";
       text = "d997dbff";
       message = "d997dbff";
       prompt = "d997dbff";
       placeholder = "f0c9f2ff";
       input = "d997dbff";
-      match = "3529aaff";
-      selection = "440c3ba0";
+      match = "4d41bfff";
+      selection = "440c3bc0";
       selection-text = "db97bcff";
-      selection-match = "3529aaff";
+      selection-match = "5a41bfff";
       border = "180028ff";
     };
   };
